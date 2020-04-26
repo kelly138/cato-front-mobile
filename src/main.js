e0be9bac775f0c654e43aca7dbbe659c5c818b75
Bubble K <40594275+kelly138@users.noreply.github.com>
2020-04-26 18:04:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md#babelpolyfill
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -11,7 +10,7 @@ import store from './store'
 // 引入全局样式
 import '@/assets/css/index.scss'
 // 设置 js中可以访问 $cdn
-import {$cdn} from '@/config'
+import { $cdn } from '@/config'
 Vue.prototype.$cdn = $cdn
 
 // 全局引入按需引入UI库 vant
